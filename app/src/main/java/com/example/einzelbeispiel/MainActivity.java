3c6dc2bd29dfe44c4a6420de9a75166e26bbd905
ptw0805 <100068225+ptw0805@users.noreply.github.com>
2022-03-06 18:25:48 +0100

    Berechnung implementiert

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,15 +27,25 @@ public class MainActivity extends AppCompatActivity {
     public void sendmassage(View view) throws IOException {
         EditText editText = (EditText) findViewById(R.id.number_matrik);
         String message = editText.getText().toString();
+        TextView response = (TextView)findViewById(R.id.server_response);
 
-        server(message);
+
+        if(server(message)){
+            String result = clickbait(message);
+            System.out.println(result);
+            response.setText(result);
+        }else{
+
+            response.setText("Dies ist keine gueltige Matrikelnummer");
+        }
+
 
 
 
     }
 
 
-    public void server(String matno) throws IOException {
+    public boolean server(String matno) throws IOException {
 
 
 
@@ -44,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
             String sentence;
             String modifiedSentence;
             Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-            System.out.println("Test1");
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -54,13 +65,19 @@ public class MainActivity extends AppCompatActivity {
             outToServer.writeBytes(sentence + "\n");
 
             modifiedSentence = inFromServer.readLine();
+            if(modifiedSentence=="Dies ist keine gueltige Matrikelnummer"){
+                return false;
+            }
 
             System.out.println("FROM SERVER: " + modifiedSentence);
 
             clientSocket.close();
+
+            return true;
         }
         catch (Exception e){
             e.printStackTrace();
+            return false;
         }
 
 
@@ -68,6 +85,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public String clickbait(String matno){
+
+        int [] click = new int[matno.length()];
+        int numbers =0;
+        for(int i = 0; i < click.length; i++){
+            int number = Integer.parseInt(String.valueOf(matno.charAt(i)));
+            if(number!= 1 && number!= 2 && number!= 3 && number!= 5 && number!= 7){
+                click[i] = number;
+                numbers++;
+                System.out.println(number);
+            }
+            else{
+                click[i] = 9;
+            }
+
+        }
+        Arrays.sort(click);
+
+        int[] result=new int[numbers];
+        for(int j = 0; j<numbers; j++){
+            result[j] = click [j];
+
+        }
+
+        return Arrays.toString(result);
+    }
 
 
 }
